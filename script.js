442c5129cb9eb01e603e655467be772ad6d62159
Abaone <ahmadfils257@gmail.com>
2025-04-02 11:31:08 +0200

    styling

--- script.js
+++ script.js
@@ -10,35 +10,19 @@ document.querySelectorAll('.news-item').forEach(item => {
     });
 });
 
-// Pour le dropdown menu
 const dropdowns = document.querySelectorAll('.dropdown');
-dropdowns.forEach(dropdown => {
-    dropdown.addEventListener('mouseover', () => {
-        const menu = dropdown.querySelector('.dropdown-menu');
-        if (menu) {
-            menu.style.display = 'block';
-        }
-    });
-
-    dropdown.addEventListener('mouseout', () => {
-        const menu = dropdown.querySelector('.dropdown-menu');
-        if (menu) {
-            menu.style.display = 'none';
-        }
-    });
-});
-// Fonction pour gérer l'affichage du menu déroulant
 const dropdown = document.querySelectorAll('.dropdown');
 
 dropdowns.forEach(dropdown => {
     const menu = dropdown.querySelector('.dropdown-menu');
 
-    dropdown.addEventListener('mouseenter', () => {
-        menu.style.display = 'block';
-    });
-
-    dropdown.addEventListener('mouseleave', () => {
-        menu.style.display = 'none';
+    dropdown.addEventListener('click', () => {
+        if (menu.style.display === 'none') {
+            menu.style.display = 'block';
+        }
+        else {
+            menu.style.display = 'none';
+        }
     });
 });
 
